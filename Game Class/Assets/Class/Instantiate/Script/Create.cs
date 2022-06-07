b583b0d407b1a5d8ef5fa62a331f1f544442e135
BaeGWoo <gwbae4211@naver.com>
2022-06-07 21:44:51 +0900

    Object Pool System Setting

--- Game Class/Assets/Class/Instantiate/Script/Create.cs
+++ Game Class/Assets/Class/Instantiate/Script/Create.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Create : MonoBehaviour
 {
-    public GameObject obj;
+    public GameObject Prefab;
 
     //Stack        Heap
                    //[]
@@ -15,19 +15,14 @@ public class Create : MonoBehaviour
     //그러면 ptr만 남게되는데 이 때 
     //*ptr=10;이라고 하면 이미 해제된 메모리 영역을 가리키는 포인터(댕글링 포인터)이기 때문에 터지게 된다.
 
-    // Start is called before the first frame update
-    void Start()
+ 
+
+ public void PrefabCreate() 
     {
         //생성할 오브젝트,  위치 , 회전(Quaternion.identity=>회전값이 0,0,0으로 초기화하는것을 의미)
-        for (int i = 1; i <= 3; i++)
-        {
-            Instantiate(obj, new Vector3(0+3*i, 0, 0), Quaternion.identity);
-        }
-    }
+        //Instantiate(Prefab, new Vector3(0, 5, 0), Quaternion.identity);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        ObjectPool.objpool.GetQueue();
+
     }
 }
--- Game Class/Assets/Class/Instantiate/Script/Dead.cs
+++ Game Class/Assets/Class/Instantiate/Script/Dead.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Dead : MonoBehaviour
 {
- 
-    void Update()
+    Rigidbody rigid;
+
+    private void Start()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
-        {
-            Destroy(this.gameObject);
-        }
+        rigid = GetComponent<Rigidbody>();
+    }
+    //오브젝트가 화면밖으로 나갔을 경우 이벤트를 발생시키는 함수
+    private void OnBecameInvisible()
+    {
+        rigid.velocity = Vector3.zero;
+        gameObject.transform.position = new Vector3(0,5,0);
+        ObjectPool.objpool.InsertQueue(gameObject);
+        
     }
 }
